c9951a54383892edc8e935c112b0fea6ac92cdd2
metadream <app@zerg.cc>
2024-02-07 20:47:41 +0800

    remove Interceptor

--- src/main/java/com/arraywork/deps/BootApplication.java
+++ src/main/java/com/arraywork/deps/BootApplication.java
@@ -1,11 +1,6 @@
 package com.arraywork.deps;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.arraywork.deps.security.Authenticator;
 
 /**
  * Web Boot Application
@@ -14,14 +9,4 @@ import com.arraywork.deps.security.Authenticator;
  * @since 2024/02/05
  */
 @SpringBootApplication(scanBasePackages = "com.arraywork.deps")
-public class BootApplication implements WebMvcConfigurer {
-
-    @Autowired
-    private Authenticator authenticator;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticator).addPathPatterns("/**");
-    }
-
-}
+public class BootApplication {}

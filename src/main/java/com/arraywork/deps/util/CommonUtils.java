e6078050e948682592bcfe2888d03acad7f0525f
metadream <app@zerg.cc>
2024-02-09 18:16:07 +0800

    Manually trigger validation

---
+++ src/main/java/com/arraywork/deps/util/CommonUtils.java
@@ -0,0 +1,26 @@
+package com.arraywork.deps.util;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+/**
+ * Common Utilities
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @since 2024/02/09
+ */
+public class CommonUtils {
+
+    // Manually trigger validation
+    public static <T> List<String> validate(T t, String property) {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<T>> errors = validator.validateProperty(t, property);
+        return errors.stream().map(e -> e.getMessage()).collect(Collectors.toList());
+    }
+
+}

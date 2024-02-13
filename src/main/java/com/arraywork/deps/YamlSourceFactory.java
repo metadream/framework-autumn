5caa427720ac87afbd15536d2b190237dac7384e
metadream <app@zerg.cc>
2024-02-13 14:36:26 +0800

    Create YamlSourceFactory.java

---
+++ src/main/java/com/arraywork/deps/YamlSourceFactory.java
@@ -0,0 +1,30 @@
+package com.arraywork.deps;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
+import org.springframework.core.io.support.EncodedResource;
+
+/**
+ * YAML Property Source Factory
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @since 2024/02/13
+ */
+public class YamlSourceFactory extends DefaultPropertySourceFactory {
+
+    @Override
+    public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+        if (resource == null) {
+            super.createPropertySource(name, resource);
+        }
+        Resource res = resource.getResource();
+        List<PropertySource<?>> sources = new YamlPropertySourceLoader().load(res.getFilename(), res);
+        return sources.get(0);
+    }
+
+}

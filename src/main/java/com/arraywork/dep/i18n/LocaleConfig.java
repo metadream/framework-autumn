765523c50974c9cb782d891f8b3a610de34eaf70
metadream <app@zerg.cc>
2024-02-26 17:04:26 +0800

    add i18n

---
+++ src/main/java/com/arraywork/dep/i18n/LocaleConfig.java
@@ -0,0 +1,40 @@
+package com.arraywork.dep.i18n;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+/**
+ * Locale Configuration
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @created 2024/02/26
+ *
+ * # application.properties
+ * spring.messages.basename = i18n/messages
+ * spring.messages.fallbackToSystemLocale = false
+ */
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+
+}

1874c0671ff15766775edf9a83fbb2e9229b96b7
metadream <app@zerg.cc>
2024-02-27 09:05:27 +0800

    fixed generic enum

--- src/main/java/com/arraywork/springshot/serialize/GenericEnum.java
+++ src/main/java/com/arraywork/springshot/serialize/GenericEnum.java
@@ -1,7 +1,13 @@
 package com.arraywork.springshot.serialize;
 
+/**
+ * Generic Enumeration
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @since 2024/02/26
+ */
 public interface GenericEnum<T> {
 
-    T getValue();
+    T getCode();
 
 }
--- src/main/java/com/arraywork/springshot/serialize/GenericEnumConverter.java
+++ src/main/java/com/arraywork/springshot/serialize/GenericEnumConverter.java
@@ -3,22 +3,25 @@ package com.arraywork.springshot.serialize;
 import java.lang.reflect.ParameterizedType;
 
 import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
 
-@Converter(autoApply = true)
-public class GenericEnumConverter<T extends GenericEnum<E>, E>
-    implements AttributeConverter<T, E> {
+/**
+ * Generic Enumeration Converter
+ * @author AiChen
+ * @copyright ArrayWork Inc.
+ * @since 2024/02/26
+ */
+public class GenericEnumConverter<T extends GenericEnum<E>, E> implements AttributeConverter<T, E> {
 
     @Override
     public E convertToDatabaseColumn(T attribute) {
-        return (attribute != null) ? attribute.getValue() : null;
+        return (attribute != null) ? attribute.getCode() : null;
     }
 
     @Override
-    public T convertToEntityAttribute(E value) {
+    public T convertToEntityAttribute(E code) {
         Class<T> clazz = getEnumClass();
         for (T e : clazz.getEnumConstants()) {
-            if (e.getValue().equals(value)) return e;
+            if (e.getCode().equals(code)) return e;
         }
         return null;
     }

1d71b826e25296f70de2b3b6231f85e22dc9ce13
metadream <app@zerg.cc>
2024-02-27 13:56:56 +0800

    Update BaseApplication.java

--- src/main/java/com/arraywork/springshot/BaseApplication.java
+++ src/main/java/com/arraywork/springshot/BaseApplication.java
@@ -3,7 +3,7 @@ package com.arraywork.springshot;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Base Boot Application
+ * SringBoot Base Application
  * @author AiChen
  * @copyright ArrayWork Inc.
  * @since 2024/02/05

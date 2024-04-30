2922eec2c94241a35dad238081d69f84e0f9088c
metadream <app@zerg.cc>
2024-04-30 19:55:02 +0800

    Update SseChannel.java

--- src/main/java/com/arraywork/springforce/SseChannel.java
+++ src/main/java/com/arraywork/springforce/SseChannel.java
@@ -1,15 +1,10 @@
 package com.arraywork.springforce;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import com.arraywork.springforce.util.KeyGenerator;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Server-Sent Event Service
@@ -18,50 +13,29 @@ import lombok.extern.slf4j.Slf4j;
  * @since 2024/04/28
  */
 @Component
-@Slf4j
 public class SseChannel {
 
-    private static final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
+    // Accept client
     public SseEmitter subscribe() {
-        String id = KeyGenerator.nanoId();
         SseEmitter emitter = new SseEmitter(0L);
-        emitter.onTimeout(() -> {
-            log.warn("Channel '{}' is timeout.", id);
-            emitters.remove(id);
-        });
-        emitter.onCompletion(() -> {
-            log.warn("Channel '{}' is completed.", id);
-            emitters.remove(id);
-        });
-        emitter.onError(e -> {
-            log.error("Channel '{}' error: ", id);
-            emitters.remove(id);
-        });
-        emitters.put(id, emitter);
+        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onError(e -> emitters.remove(emitter));
+        emitters.add(emitter);
         return emitter;
     }
 
-    public void publish(String id, Object object) {
-        SseEmitter emitter = emitters.get(id);
-        if (emitter != null) {
+    // Broadcast message to all clients
+    public void broadcast(Object object) {
+        for (SseEmitter emitter : emitters) {
             try {
                 emitter.send(object);
-            } catch (IOException e) {
+            } catch (Exception e) {
                 emitter.completeWithError(e);
             }
         }
-    }
-
-    // TODO foreach remove元素风险
-    public void publish(Object object) {
-        emitters.forEach((id, emitter) -> {
-            try {
-                emitter.send(object);
-            } catch (IOException e) {
-                emitter.completeWithError(e);
-            }
-        });
     }
 
 }

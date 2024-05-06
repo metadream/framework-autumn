5afdea278a81500426264dff2ae4361c72d7af9d
metadream <app@zerg.cc>
2024-05-06 17:48:26 +0800

    Update Files.java

--- src/main/java/com/arraywork/springforce/util/Files.java
+++ src/main/java/com/arraywork/springforce/util/Files.java
@@ -3,7 +3,6 @@ package com.arraywork.springforce.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.core.io.InputStreamSource;
@@ -28,16 +27,6 @@ public class Files {
         if (filename == null) return null;
         int i = filename.lastIndexOf(".");
         return i > -1 ? filename.substring(i) : "";
-    }
-
-    // Get the length of filename
-    public static int getNameLength(String filename) {
-        try {
-            return filename.getBytes("UTF-8").length;
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return -1;
     }
 
     // Walk directory and add files to argument List<File>
